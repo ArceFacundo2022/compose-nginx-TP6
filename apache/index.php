1766aaec06fe798db4b9d2a05e38987ba7d8e1d4
ArceFacundo2022 <17luckgames17@gmail.com>
2023-09-20 15:44:07 -0300

    compose nginx

---
+++ apache/index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Compose Apache - NODE</title>
+</head>
+<body>
+
+<h1>Contenedor APACHE - NODE Numero 1</h1>
+
+</body>
+</html>
---
+++ apache2/index.php
@@ -0,0 +1,12 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Compose Apache - NODE</title>
+</head>
+<body>
+
+<h1>Contenedor APACHE - NODE Numero 2</h1>
+
+</body>
+</html>
